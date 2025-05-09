0d19c8717adfdafc72b0a967c9713537ef1abcf8
Tomokazu Katayama <poko.peko.tomo@icloud.com>
2025-05-10 03:14:52 +0900

    update.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::{HashMap, HashSet};
 use std::hash::{Hash, Hasher};
 use sqlx::ConnectOptions;
 use sqlx::sqlite::SqliteConnectOptions;
+use url::Url;
 
 #[derive(Debug)]
 #[derive(Eq, PartialEq)]
@@ -16,16 +17,20 @@ impl Hash for VideoData {
         self.video_id.hash(state);
     }
 }
+
+async fn list_playlist_items(key: String) {
+    let query_url = Url::parse_with_params("https://www.googleapis.com/youtube/v3/playlistItems", &[("", "")]).unwrap();
+}
 #[tokio::main]
 async fn main() {
     let mut research_table: HashMap<String, HashSet<VideoData>> = HashMap::new();
     let mut db = SqliteConnectOptions::new().filename("data.sqlite").connect().await.unwrap();
-    let _ = sqlx::query_as("SELECT tbl_name FROM sqlite_master WHERE type = 'table';")
-        .fetch_all(&mut db).await.unwrap().into_iter().for_each(|(row, ): (String,)| {
-        if row.starts_with("__") && row.ends_with("__") {
-            return;
-        }
-        research_table.insert(row, HashSet::new());
+    sqlx::query_as("SELECT tbl_name FROM sqlite_master WHERE type = 'table';")
+        .fetch_all(&mut db).await.unwrap().into_iter().filter_map(|(row, ): (String,)| {
+        if row.starts_with("__") && row.ends_with("__") { None } else { Some(row) }
+    }).chain(sqlx::query_as("SELECT db_key FROM __source__;")
+        .fetch_all(&mut db).await.unwrap().into_iter().map(|(row, ): (String,)| { row })).for_each(|key| {
+        research_table.insert(key, HashSet::new());
     });
     for (table_name, table_data) in research_table.iter_mut() {
         for video_id in sqlx::query_as("SELECT name FROM pragma_table_info(?);").bind(table_name).fetch_all(&mut db).await.unwrap().into_iter().skip(1).map(|(video_id, ): (String,)| { video_id }) {
