7cabbe64cdcc6dbd3ddb83dc75df60120f0d1c35
yayoimizuha <poko.peko.tomo@icloud.com>
2025-05-09 14:23:59 +0900

    update

--- src/bin/migrate_db.rs
+++ src/bin/migrate_db.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
 use sqlx::sqlite::SqliteConnectOptions;
-use sqlx::ConnectOptions;
+use sqlx::{Acquire, ConnectOptions};
 
 #[tokio::main]
 async fn main() {
@@ -17,17 +17,41 @@ async fn main() {
     let table_names = sqlx::query_as("SELECT tbl_name FROM sqlite_master WHERE type = 'table';")
         .fetch_all(&mut py_db).await.unwrap().iter().map(|(tbl_name, ): &(String,)| tbl_name.to_owned()).collect::<Vec<_>>();
     for table in table_names {
-        sqlx::query(format!("CREATE TABLE '{}' ('index' DATE PRIMARY KEY NOT NULL);",table).as_str()).execute(&mut rust_db).await.unwrap();
+        sqlx::query(format!("CREATE TABLE '{}' ('index' DATE PRIMARY KEY NOT NULL);", table).as_str()).execute(&mut rust_db).await.unwrap();
 
-        let rows = sqlx::query_as("SELECT name FROM pragma_table_info(?);").bind(&table)
-            .fetch_all(&mut py_db).await.unwrap().iter().map(|(index_name, ): &(String,)| { index_name.to_owned() }).collect::<Vec<_>>();
-        println!("{}", table);
+        // let rows = sqlx::query_as("SELECT name FROM pragma_table_info(?);").bind(&table)
+        //     .fetch_all(&mut py_db).await.unwrap().iter().map(|(index_name, ): &(String,)| { index_name.to_owned() }).collect::<Vec<_>>();
+        // println!("{}", table);
         for (index, title) in sqlx::query_as(format!("SELECT \"index\",\"タイトル\" FROM '{}';", table).as_str())
             .fetch_all(&mut py_db).await.unwrap().iter().map(|(index, title, ): &(String, String)| { (index.to_owned(), title.to_owned()) }) {
             println!("{}:{}", index, title);
             sqlx::query("INSERT OR REPLACE INTO __title__(youtube_id,title) VALUES (?,?);")
                 .bind(index.strip_prefix("https://youtu.be/").unwrap()).bind(title).execute(&mut rust_db).await.unwrap();
+            sqlx::query(format!("ALTER TABLE '{}' ADD COLUMN '{}' INTEGER;", table, index.strip_prefix("https://youtu.be/").unwrap()).as_str()).execute(&mut rust_db).await.unwrap();
         }
+        let mut transaction = rust_db.begin().await.unwrap();
+        for date in sqlx::query_as("SELECT name FROM pragma_table_info(?);").bind(&table)
+            .fetch_all(&mut py_db).await.unwrap().iter().map(|(index_name, ): &(String,)| { index_name.to_owned() }).skip(2) {
+            sqlx::query(format!("INSERT INTO '{}'('index') VALUES(datetime(?))", table).as_str()).bind({
+                if date.contains(":") {
+                    date.replace("00:00:00", "08:00:00")
+                } else {
+                    format!("{} 08:00:00", date)
+                }
+            }).execute(&mut *transaction).await.unwrap();
+
+            for (index, count) in sqlx::query_as(format!(r##"SELECT "index","{date}" FROM "{table}" ;"##).as_str())
+                .fetch_all(&mut py_db).await.unwrap().iter().map(|(index, count): &(String, Option<i64>)| { (index.to_owned(), count.to_owned()) }) {
+                sqlx::query(format!(r##"UPDATE "{table}" SET "{}" = ? WHERE "index"=?;"##, index.strip_prefix("https://youtu.be/").unwrap()).as_str()).bind(count).bind({
+                    if date.contains(":") {
+                        date.replace("00:00:00", "08:00:00")
+                    } else {
+                        format!("{} 08:00:00", date)
+                    }
+                }).execute(&mut *transaction).await.unwrap();
+            }
+        }
+        transaction.commit().await.unwrap();
 
         // for row in rows.iter().skip(2) {
         //     // println!("{}", row);
@@ -37,3 +61,4 @@ async fn main() {
         // }
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    
+use std::collections::HashMap;
+use sqlx::ConnectOptions;
+use sqlx::sqlite::SqliteConnectOptions;
+
+#[derive(Debug)]
+struct VideoData {
+    video_id: String,
+    title: Option<String>,
+    views: Option<i64>,
+}
+#[tokio::main]
+async fn main() {
+    let mut research_table: HashMap<String, HashMap<String, VideoData>> = HashMap::new();
+    let mut db = SqliteConnectOptions::new().filename("data.sqlite").connect().await.unwrap();
+    let _ = sqlx::query_as("SELECT tbl_name FROM sqlite_master WHERE type = 'table';")
+        .fetch_all(&mut db).await.unwrap().into_iter().for_each(|(row, ): (String,)| {
+        if row.starts_with("__") && row.ends_with("__") {
+            return;
+        }
+        research_table.insert(row, HashMap::new());
+    });
+    println!("{:?}", research_table);
 }
