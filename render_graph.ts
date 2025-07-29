10eec99572a63ff5b9854d1666f1d9afc275b909
yayoimizuha <poko.peko.tomo@icloud.com>
2025-07-29 13:48:08 +0900

    update

--- render_graph.ts
+++ render_graph.ts
@@ -1,32 +1,91 @@
-import {configurationOptionDescriptions, DuckDBInstance, version as duckdb_version,} from "npm:@duckdb/node-api";
+// noinspection SqlNoDataSourceInspection
 
+import {
+    // configurationOptionDescriptions,
+    DuckDBInstance,
+    // version as duckdb_version,
+    DuckDBTimestampTZValue,
+    // DuckDBValue
+} from 'npm:@duckdb/node-api';
 import * as echarts from 'npm:echarts';
-import {DuckDBValue} from "npm:@duckdb/node-api@1.3.2-alpha.25";
+import {EChartsOption, SeriesOption} from 'npm:echarts';
+import moment from 'npm:moment';
+import * as fs from 'node:fs';
 
-// console.log("DuckDB version: " + duckdb_version());
-// console.log(configurationOptionDescriptions());
-const duckdb_instance = await DuckDBInstance.create("data.duckdb");
+const duckdb_instance = await DuckDBInstance.create('data.duckdb');
 
 const duckdb_connection = await duckdb_instance.connect();
 
+
 const echarts_instance = echarts.init(null, null, {
-    renderer: "svg",
+    renderer: 'svg',
     ssr: true,
     width: 4000,
     height: 2500
 });
 
-(await (await duckdb_connection.run("SHOW TABLES;")).getRows()).map(async ([table_name]) => {
-    const column_names = await (await duckdb_connection.run("SELECT name FROM pragma_table_info(?) WHERE name <> 'index';", [table_name])).getRows();
-    console.log("Table: " + table_name);
-    if (!(table_name).toString().startsWith('__') || !(table_name).toString().endsWith('__')) {
-        const date_index = (await (await duckdb_connection.run(`SELECT index
-                                                                FROM \"${table_name}\"`)).getRows()).map(([v]) => v);
-        column_names.map(([column_name]) => {
-                console.log(column_name)
+for (const [table_name] of (await (await duckdb_connection.run('SELECT table_name FROM information_schema.tables WHERE NOT STARTS_WITH(table_name,"__") AND NOT ENDS_WITH(table_name,"__");')).getRows())) {
+    // if (table_name != 'BEYOOOOONDS') continue
+
+    const column_names = (await (await duckdb_connection.run('SELECT name FROM pragma_table_info(?);', [table_name])).getRows()).map(([v]) => v as string)
+    console.log(`Table: ${table_name}`);
+    console.log(column_names);
+    const data = (await (await duckdb_connection.run('SELECT * FROM query_table(?)', [table_name])).getRows()).map(row => row.map(v => {
+        if (v instanceof DuckDBTimestampTZValue) {
+            return Date.parse(v.toString())
+        } else if (typeof v == 'number') {
+            return v
+        } else {
+            return null
+        }
+    }));
+    const series: SeriesOption[] = column_names.slice(1).map((column_name) => {
+        return {
+            name: column_name,
+            type: 'line',
+            smooth: true,
+            encode: {
+                x: 'index',
+                y: column_name
             }
-        )
+        }
+    })
+
+    const chart_option: EChartsOption = {
+        dataset: {
+            source: data,
+            dimensions: column_names
+        },
+        xAxis: {
+            type: 'time',
+            axisLabel: {
+                formatter(value, _index, _extra) {
+                    return moment(value).format('YYYY/MM/DD');
+                },
+            }
+
+        },
+        yAxis: {
+            min: 0,
+            position: 'left',
+            axisLabel: {
+                formatter(value: number) {
+                    return `${Math.floor(value / 10000)}万回`
+                }
+            }
+        },
+        series: series
     }
+    chart_option && echarts_instance.setOption(chart_option);
+
+    const chart_svg = echarts_instance.renderToSVGString()
+    fs.writeFileSync(`${table_name}.svg`, chart_svg);
 
 
-});
+    echarts_instance.clear();
+}
+
+duckdb_connection.closeSync()
+duckdb_instance.closeSync()
+echarts_instance.clear()
+echarts_instance.dispose()
