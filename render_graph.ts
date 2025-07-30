2d92b4c992cc3773ed103184e09f7234b885ea9c
Tomokazu Katayama <poko.peko.tomo@icloud.com>
2025-07-30 09:44:32 +0900

    update

--- render_graph.ts
+++ render_graph.ts
@@ -16,7 +16,12 @@ const duckdb_instance = await DuckDBInstance.create('data.duckdb');
 
 const duckdb_connection = await duckdb_instance.connect();
 
-
+const bgColor: echarts.Color = '#f0f0f0';
+const defaultFont = {
+    fontFamily: 'Noto Sans JP',
+    fontSize: 20,
+    fontWeight: 'Regular'
+}
 const echarts_instance = echarts.init(null, null, {
     renderer: 'svg',
     ssr: true,
@@ -25,23 +30,26 @@ const echarts_instance = echarts.init(null, null, {
 });
 
 for (const [table_name] of (await (await duckdb_connection.run('SELECT table_name FROM information_schema.tables WHERE NOT STARTS_WITH(table_name,\'__\') AND NOT ENDS_WITH(table_name,\'__\');')).getRows())) {
-    if (table_name != 'BEYOOOOONDS') continue
+    if ((table_name != 'BEYOOOOONDS') && (table_name != 'モーニング娘。')) continue
 
     const column_names = (await (await duckdb_connection.run('SELECT name FROM pragma_table_info(?);', [table_name])).getRows()).map(([v]) => v as string)
     console.log(`Table: ${table_name}`);
     console.log(column_names);
+    let max_count: number = -Infinity;
     const data = (await (await duckdb_connection.run('SELECT * FROM query_table(?)', [table_name])).getRows()).map(row => row.map(v => {
         if (v instanceof DuckDBTimestampTZValue) {
             return Date.parse(v.toString())
         } else if (typeof v == 'number') {
+            max_count = Math.max(v, max_count)
             return v
         } else {
             return null
         }
     }));
-    const series: SeriesOption[] = column_names.slice(1).map((column_name) => {
+    const series: SeriesOption[] = await Promise.all(column_names.slice(1).map((async (column_name) => {
+        const title = (await (await duckdb_connection.run('SELECT cleaned_title FROM __title__ WHERE youtube_id = ?', [column_name])).getRows()).at(0)!.at(0)!.toString();
         return {
-            name: column_name,
+            name: title || '',
             type: 'line',
             smooth: true,
             encode: {
@@ -49,9 +57,13 @@ for (const [table_name] of (await (await duckdb_connection.run('SELECT table_nam
                 y: column_name
             }
         }
-    })
-
+    })))
     const chart_option: EChartsOption = {
+        title: {
+            left:'center',
+            text: (await (await duckdb_connection.run('SELECT DISTINCT screen_name FROM __source__ WHERE db_key = ? ORDER BY playlist_key;', [table_name])).getRows()).at(0)!.at(0)!.toString() || '',
+        },
+        backgroundColor: bgColor,
         dataset: {
             source: data,
             dimensions: column_names
@@ -63,6 +75,22 @@ for (const [table_name] of (await (await duckdb_connection.run('SELECT table_nam
                     return moment(value).format('YYYY/MM/DD');
                 },
                 rotate: 30
+            }
+        },
+        grid: {
+            right: 400,
+            left: 100,
+        },
+        legend: {
+            type: 'scroll',
+            orient: 'vertical',
+            align: 'left',
+            right: 20,
+            top: 20,
+            pageIconColor: bgColor,
+            pageIconInactiveColor: bgColor,
+            pageTextStyle: {
+                color: bgColor
             }
         },
         yAxis: {
