0e3f05b579ced1662e51284b1dcdbefdfb81bd16
yayoimizuha <poko.peko.tomo@icloud.com>
2025-08-07 09:01:07 +0900

    update

--- src/bin/duckdb_test.rs
+++ src/bin/duckdb_test.rs
@@ -2,7 +2,7 @@ use duckdb::{params, Connection};
 #[tokio::main]
 async fn main() {
     let duckdb = Connection::open("data.duckdb").unwrap();
-    println!("aaaa");
+    // println!("aaaa");
     let _ = duckdb.prepare("SELECT raw_title FROM __title__ WHERE youtube_id = (?)").unwrap().query_map(params!["FJfife3J8uM".to_owned()], |row| {
         let raw_title = row.get::<_,Option<String>>(0).unwrap();
         println!("raw_title: {:?}", raw_title);
--- src/bin/instagram_scraper.rs
+++ src/bin/instagram_scraper.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use duckdb::{params, Connection};
+
+#[tokio::main]
+async fn main() {
+    let duckdb = Connection::open("data.duckdb").unwrap();
+    duckdb.prepare("SELECT table_name FROM information_schema.tables WHERE NOT STARTS_WITH(table_name,'__') AND NOT ENDS_WITH(table_name,'__');").unwrap().query_map(params![], |row| {
+        let db_name: String = row.get(0).unwrap();
+        println!("Database: {}", db_name);
+        Ok(())
+    }).unwrap().count();
+}
