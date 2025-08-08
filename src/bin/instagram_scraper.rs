68b246a42a3390b825e0231c8a10f5f2c5e2b0cf
yayoimizuha <poko.peko.tomo@icloud.com>
2025-08-08 17:50:32 +0900

    update

--- src/bin/instagram_scraper.rs
+++ src/bin/instagram_scraper.rs
@@ -1,11 +1,30 @@
+use std::collections::HashSet;
+use std::fs;
 use duckdb::{params, Connection};
 
 #[tokio::main]
 async fn main() {
-    let duckdb = Connection::open("data.duckdb").unwrap();
-    duckdb.prepare("SELECT table_name FROM information_schema.tables WHERE NOT STARTS_WITH(table_name,'__') AND NOT ENDS_WITH(table_name,'__');").unwrap().query_map(params![], |row| {
-        let db_name: String = row.get(0).unwrap();
-        println!("Database: {}", db_name);
-        Ok(())
-    }).unwrap().count();
+    let duckdb = Connection::open("misc.duckdb").unwrap();
+    duckdb.execute("CREATE TABLE IF NOT EXISTS instagram_accounts (id TEXT PRIMARY KEY);", params![]).unwrap();
+    let mut usernames: HashSet<String> = duckdb.prepare("SELECT * FROM instagram_accounts;").unwrap().query_map(params![], |row| {
+        Ok(row.get::<_, String>(0).unwrap())
+    }).unwrap().filter_map(|v| { v.ok() }).collect();
+
+    if fs::exists("instagram_user.list").unwrap() {
+        // gather usernames from the file without started with #
+        usernames = fs::read_to_string("instagram_user.list")
+            .unwrap()
+            .lines()
+            .filter_map(|line| {
+                let line = line.trim();
+                if line.is_empty() || line.starts_with('#') {
+                    None
+                } else {
+                    duckdb.execute("INSERT OR IGNORE INTO instagram_accounts (id) VALUES (?);", params![line]).unwrap();
+                    Some(line.to_string())
+                }
+            }).collect();
+    }
+    println!("accounts={:?}", usernames.into_iter().collect::<Vec<String>>());
 }
+
