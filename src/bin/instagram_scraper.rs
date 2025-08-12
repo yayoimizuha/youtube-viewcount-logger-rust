41867b8e057188987904b3bbc2f09b1a901838de
yayoimizuha <poko.peko.tomo@icloud.com>
2025-08-12 18:04:39 +0900

    update

--- src/bin/instagram_scraper.rs
+++ src/bin/instagram_scraper.rs
@@ -1,7 +1,25 @@
 use std::collections::HashSet;
 use std::fs;
 use duckdb::{params, Connection};
+use reqwest::Client;
 
+const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0";
+fn detect_json_end(string: String) -> Option<usize> {
+    let mut depth = 0;
+    for (i, c) in string.chars().enumerate() {
+        match c {
+            '{' => depth += 1,
+            '}' => {
+                if depth == 0 {
+                    return Some(i);
+                }
+                depth -= 1;
+            }
+            _ => {}
+        }
+    }
+    None
+}
 #[tokio::main]
 async fn main() {
     let duckdb = Connection::open("misc.duckdb").unwrap();
@@ -12,7 +30,7 @@ async fn main() {
 
     if fs::exists("instagram_user.list").unwrap() {
         // gather usernames from the file without started with #
-        usernames = fs::read_to_string("instagram_user.list")
+        usernames.extend(fs::read_to_string("instagram_user.list")
             .unwrap()
             .lines()
             .filter_map(|line| {
@@ -23,8 +41,18 @@ async fn main() {
                     duckdb.execute("INSERT OR IGNORE INTO instagram_accounts (id) VALUES (?);", params![line]).unwrap();
                     Some(line.to_string())
                 }
-            }).collect();
+            }));
     }
-    println!("accounts={:?}", usernames.into_iter().collect::<Vec<String>>());
+    let client = Client::builder().user_agent(USER_AGENT).build().unwrap();
+    for username in usernames {
+        println!("{}", username);
+        let document = client.get(format!("https://www.instagram.com/{username}/embed")).send().await.unwrap().text().await.unwrap();
+        fs::write("document.html", &document).unwrap();
+        let json_start_pos = document.find("contextJSON").unwrap() + 15;
+        let json_end_pos = detect_json_end(document[json_start_pos..].to_string()).unwrap() + json_start_pos;
+        let json_str = &document[json_start_pos..json_end_pos];
+        println!("{}", json_str);
+        break;
+    }
 }
 
